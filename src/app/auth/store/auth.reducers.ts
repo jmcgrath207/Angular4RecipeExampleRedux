1f8b2406fea3d0bc7eae0577f153f3dda67a10ea
sonance207 <sonance207@gmail.com>
2017-10-07 23:07:36 -0500

    adding reducer logic

--- src/app/auth/store/auth.reducers.ts
+++ src/app/auth/store/auth.reducers.ts
@@ -1,4 +1,6 @@
 
+import * as AuthActions from './auth.actions';
+
 export interface State {
   token: string;
   authenticated: boolean;
@@ -10,7 +12,21 @@ const initialState: State = {
   authenticated: false
 };
 
-export function authReducer(state = initialState, action) {
-
-  return state;
+export function authReducer(state = initialState, action: AuthActions.AuthActions) {
+  switch (action.type) {
+    case (AuthActions.SIGNUP):
+    case(AuthActions.SIGNIN):
+      return {
+        ...state,
+        authenticated: true
+      };
+    case (AuthActions.LOGOUT):
+      return {
+        ...state,
+        token: null,
+        authenticated: false
+      };
+    default:
+      return state;
+  }
 }

7ee14dbd55c2a68aa8ae7c7a34a5b83f23c0632d
sonance207 <sonance207@gmail.com>
2017-10-08 12:26:33 -0500

    auth effects and actions

--- src/app/auth/store/auth.actions.ts
+++ src/app/auth/store/auth.actions.ts
@@ -1,9 +1,19 @@
 import { Action } from '@ngrx/store';
 
+
+export const TRY_SIGNUP = 'TRY_SIGNUP';
 export const SIGNUP = 'SIGNUP';
 export const SIGNIN = 'SIGNIN';
 export const LOGOUT = 'LOGOUT';
 export const SET_TOKEN = 'SET_TOKEN';
+
+
+export class TrySignup implements Action {
+  readonly type = TRY_SIGNUP;
+
+  constructor(public payload: {username: string, password: string}) {}
+}
+
 
 export class Signup implements Action {
   readonly type = SIGNUP;
@@ -25,4 +35,4 @@ export class SetToken implements Action {
 
 }
 
-export type AuthActions = Signup | Signin | Logout | SetToken;
+export type AuthActions = Signup | Signin | Logout | SetToken | TrySignup;
--- src/app/auth/store/auth.effects.ts
+++ src/app/auth/store/auth.effects.ts
@@ -1,13 +1,15 @@
 
-
 import {Actions, Effect} from '@ngrx/effects';
 import {Injectable} from '@angular/core';
+
+import * as AuthActions from './auth.actions';
+
 
 @Injectable()
 export class AuthEffects {
 
   @Effect()
-  authSignup;
+  authSignup = this.actions$.ofType(AuthActions.TRY_SIGNUP);
 
   constructor(private actions$: Actions ) {}
 

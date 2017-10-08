ff537e6ac792d77b9e3839b36ed079d51a6d3127
sonance207 <sonance207@gmail.com>
2017-10-08 12:54:45 -0500

    325

--- src/app/auth/store/auth.effects.ts
+++ src/app/auth/store/auth.effects.ts
@@ -3,13 +3,35 @@ import {Actions, Effect} from '@ngrx/effects';
 import {Injectable} from '@angular/core';
 
 import * as AuthActions from './auth.actions';
-
+import 'rxjs/add/operator/switchMap';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/mergeMap';
+import * as firebase from 'firebase';
+import  { fromPromise} from 'rxjs/observable/fromPromise';
 
 @Injectable()
 export class AuthEffects {
 
   @Effect()
-  authSignup = this.actions$.ofType(AuthActions.TRY_SIGNUP);
+  authSignup = this.actions$.ofType(AuthActions.TRY_SIGNUP)
+    .map((action: AuthActions.TrySignup) => {
+    return action.payload;
+      })
+    .switchMap((authData: {username: string, password: string}) => {
+    return fromPromise(firebase.auth().createUserWithEmailAndPassword(authData.username, authData.password));
+    }).switchMap( () => {
+      return fromPromise(firebase.auth().currentUser.getToken());
+    }).mergeMap( (token: string) => {
+      return [
+        {
+          type: AuthActions.SIGNUP
+        },
+        {
+          type: AuthActions.SET_TOKEN,
+          payload: token
+        }
+        ];
+    });
 
   constructor(private actions$: Actions ) {}
 

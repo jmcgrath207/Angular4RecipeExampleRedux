a177850c6afa269bb4eb9e09b1fb4203dd6f6355
sonance207 <sonance207@gmail.com>
2017-10-08 00:19:12 -0500

    getting state access in http interceptor

--- src/app/Shared/auth.interceptor.ts
+++ src/app/Shared/auth.interceptor.ts
@@ -3,19 +3,26 @@
 import {HttpEvent, HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
 import {Observable} from 'rxjs/Observable';
 import {Injectable} from '@angular/core';
-import {AuthService} from '../auth/auth.service';
 
+
+import * as fromApp from '../store/app.reducers';
+import * as fromAuth from '../auth/store/auth.reducers';
+import {Store} from '@ngrx/store';
+import 'rxjs/add/operator/switchMap';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
 
-  constructor(private authService: AuthService) {}
+  constructor(private store: Store<fromApp.AppState>) {}
 
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     console.log('Intercepted!', req);
 /*    const copiedReq = req.clone({headers: req.headers.append('', '')});*/
-    const copiedReq = req.clone({params: req.params.set('auth', this.authService.getToken())});
-    return next.handle(copiedReq);
+    return this.store.select('auth')
+      .switchMap((authState: fromAuth.State) => {
+        const copiedReq = req.clone({params: req.params.set('auth', authState.token)});
+        return next.handle(copiedReq);
+        });
   }
 
 }
--- src/app/auth/auth-guard.service.ts
+++ src/app/auth/auth-guard.service.ts
@@ -3,15 +3,20 @@ import {Injectable} from '@angular/core';
 
 
 import {ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot} from '@angular/router';
-import {AuthService} from './auth.service';
+import {Store} from '@ngrx/store';
+
+import * as fromApp from '../store/app.reducers';
+import * as fromAuth from './store/auth.reducers';
 
 @Injectable()
 export class AuthGuardService implements CanActivate {
 
-  constructor(private authService: AuthService) {}
+  constructor(private store: Store<fromApp.AppState>) {}
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-    return this.authService.isAuthenticated();
+    return this.store.select('auth').map((authState: fromAuth.State) => {
+      return authState.authenticated;
+    });
   }
 
 }

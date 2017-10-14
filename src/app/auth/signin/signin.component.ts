378c3a4e3d8dceb46984be8f24b4aebff0d0fa85
sonance207 <sonance207@gmail.com>
2017-10-14 14:56:58 -0500

    334

--- src/app/auth/signin/signin.component.ts
+++ src/app/auth/signin/signin.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import {NgForm} from '@angular/forms';
-import {AuthService} from '../auth.service';
 import * as fromApp from '../../store/app.reducers';
 import * as AuthActions from '../store/auth.actions';
 import {Store} from '@ngrx/store';
